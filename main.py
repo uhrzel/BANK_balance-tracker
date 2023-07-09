30303589bbe3279d4aed3fb2ed9d0907abb1f3e8
Arzel John Zolina <80521619+ArzelJrz10@users.noreply.github.com>
2023-07-09 16:51:06 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import datetime
+
+def update_balance_sheet():
+    # Initialize balance to 0
+    balance = 0
+    
+    try:
+        with open('C:\\Users\\User\\Documents\\textFolder\\AUB_balance.txt', 'r') as file:
+            lines = file.readlines()
+            if lines:
+             
+                last_line = lines[-1]
+                balance = int(last_line.split(': ')[1].replace(',', ''))
+    except FileNotFoundError:
+      
+        pass
+    
+    continue_process = 'Y'
+    while continue_process.upper() == 'Y':
+        deposit = input('Enter your deposit amount {} ({}): '.format(datetime.datetime.now().strftime('%m/%d/%y'), datetime.datetime.now().strftime('%I:%M %p')))
+        confirm = input('Are you sure (Y/N): ')
+        if confirm.upper() == 'Y':
+            deposit = deposit.replace(',', '')  
+            balance += int(deposit)  
+            with open('C:\\Users\\User\\Documents\\textFolder\\AUB_balance.txt', 'a') as file:
+                file.write(datetime.datetime.now().strftime('%B %d, %Y (%I:%M %p)') + ': ' + '{:,}'.format(balance) + '\n')  # Format balance with commas
+            print('Output: {}: {}'.format(datetime.datetime.now().strftime('%B %d,%Y'), '{:,}'.format(balance)))  # Format balance with commas
+        else:
+            print('Deposit operation cancelled...')
+        continue_process = input('Do you want to continue (Y/N): ')
+        if continue_process.upper() == 'N':
+            print('Your text file is updated successfully...')
+
+update_balance_sheet()
